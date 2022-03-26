bb716c76cfafefba0d0de2a47365e6ed8c7ad0ac
Makoto87 <makosinhori@gmail.com>
2022-03-26 22:18:29 +0900

    add: create global styles

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button, ChakraProvider } from '@chakra-ui/react';
+import theme from './theme/theme';
 
 function App() {
 return (
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Button>テスト</Button>
       </ChakraProvider>
     )
