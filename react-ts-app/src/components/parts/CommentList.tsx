3c7e4250f79cac2d658e785c1790e94a1715d711
Makoto87 <makosinhori@gmail.com>
2022-03-28 08:10:35 +0900

    fix: padding of Comment List was fixed to become good look

--- react-ts-app/src/components/parts/CommentList.tsx
+++ react-ts-app/src/components/parts/CommentList.tsx
@@ -6,9 +6,9 @@ export const CommentList = () => {
                   <Box bg='white' border='3px' borderWidth={5} borderColor='red.500' shadow='sm' rounded='md' w='100%'  fontSize='xl' p={5} pb={3}>
                         <Text>この番組面白すぎ！ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</Text>
                         <Flex pt={2} fontSize='md' direction={{ base: 'column', md: 'row' }} justifyContent='space-between' alignItems={{md: 'center'}} textColor='red.300'>
-                              <Text flexGrow='2' pb={{base: '1'}}>ゲストユーザー</Text>
-                              <Text flexGrow='4' pb={{base: '1'}}>2020/12/31</Text>
-                              <Flex alignItems='center' pb={{base: '2'}}>
+                              <Text flexGrow='2' pb={{base: '1', md: '0'}}>ゲストユーザー</Text>
+                              <Text flexGrow='4' pb={{base: '3', md: '0'}}>2020/12/31</Text>
+                              <Flex alignItems='center' pb={{base: '2', md: '0'}}>
                                     <Button flexGrow='1' colorScheme='red.200' variant='outline' size='sm'>Good</Button>
                                     <Text flexGrow='10' pl='3'>100</Text>
                               </Flex>
