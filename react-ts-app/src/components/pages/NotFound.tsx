2ddeccd9aff87de994e4286524b97e50938af7ed
Makoto87 <makosinhori@gmail.com>
2022-03-29 08:16:53 +0900

    change: add link to home in NotFound screen

--- react-ts-app/src/components/pages/NotFound.tsx
+++ react-ts-app/src/components/pages/NotFound.tsx
@@ -1,7 +1,14 @@
+import { Container } from "@chakra-ui/react";
 import { memo } from "react"
+import { Link } from "react-router-dom";
 
 export const NotFound = memo(() => {
       return (
-            <p>404ページです</p>
+            <>
+                  <Container>
+                        <p>404ページです</p>
+                        <Link style={{color: 'red', textDecoration: 'underline'}} to="/">Homeへ</Link>
+                  </Container>
+            </>
       )
 });
