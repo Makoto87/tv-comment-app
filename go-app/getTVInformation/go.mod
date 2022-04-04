9110b6e123e9aeb26e06c92147018095c5eda31c
Makoto87 <makosinhori@gmail.com>
2022-04-04 20:53:13 +0900

    add: goquery to get elements after chromdedp get html

--- go-app/getTVInformation/go.mod
+++ go-app/getTVInformation/go.mod
@@ -2,9 +2,13 @@ module github.com/Makoto87/tv-comment-app/go-app/getTVInformation
 
 go 1.18
 
-require github.com/chromedp/chromedp v0.8.0
+require (
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/chromedp/chromedp v0.8.0
+)
 
 require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/chromedp/cdproto v0.0.0-20220321060548-7bc2623472b3 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
@@ -12,5 +16,6 @@ require (
 	github.com/gobwas/ws v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 )
--- go-app/getTVInformation/scraping.go
+++ go-app/getTVInformation/scraping.go
@@ -4,23 +4,44 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/chromedp/chromedp"
 )
 
 func Scraping() {
-	ctx, cancel := chromedp.NewContext(context.Background())
+
+	// contextの用意
+	ctx, _ := chromedp.NewContext(context.Background())
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
 	defer cancel()
 
-	var res string
+	// htmlの取得
+	var selector = "span.epg-item_seriesTitleText__RnbO0"
+	var html string
 	if err := chromedp.Run(ctx,
-		chromedp.Navigate("https://example.com"),
-		chromedp.Sleep(time.Second),
-		chromedp.Text(`h1`, &res, chromedp.NodeVisible, chromedp.ByQuery),
+		chromedp.Navigate("https://tver.jp/program"),
+		chromedp.ScrollIntoView(`footer`),
+		chromedp.WaitVisible(selector),
+		// HTMLの取得
+		chromedp.OuterHTML("html", &html, chromedp.ByQuery),
 	); err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 
-	fmt.Printf("h1 contains: '%s'\n", res)
+	// HTMLをDOMオブジェクトへ変換
+	dom, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// DOMノードを検索し、空欄以外全て取得
+	dom.Find(selector).Each(func(i int, selection *goquery.Selection) {
+		str := selection.Text()
+		if str != "" {
+			fmt.Println(str)
+		}
+	})
 }
