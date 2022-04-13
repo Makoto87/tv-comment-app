99192f6853570b67a6df4467929e092c82e4d667
Makoto87 <makosinhori@gmail.com>
2022-04-13 22:14:42 +0900

    change: method of scraping, package. not get HTML, but get elements of program title

--- go-app/gettvinfo/go.mod
+++ go-app/gettvinfo/go.mod
@@ -4,20 +4,16 @@ go 1.18
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.0
-	github.com/chromedp/chromedp v0.8.0
+	github.com/go-rod/rod v0.105.1
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/google/go-cmp v0.5.7
 	github.com/joho/godotenv v1.4.0
 	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8
 )
 
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
-	github.com/chromedp/cdproto v0.0.0-20220321060548-7bc2623472b3 // indirect
-	github.com/chromedp/sysutil v1.0.0 // indirect
-	github.com/gobwas/httphead v0.1.0 // indirect
-	github.com/gobwas/pool v0.2.1 // indirect
-	github.com/gobwas/ws v1.1.0 // indirect
-	github.com/josharian/intern v1.0.0 // indirect
-	github.com/mailru/easyjson v0.7.7 // indirect
-	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+	github.com/ysmood/goob v0.4.0 // indirect
+	github.com/ysmood/gson v0.7.1 // indirect
+	github.com/ysmood/leakless v0.7.0 // indirect
 )
--- go-app/gettvinfo/scraping.go
+++ go-app/gettvinfo/scraping.go
@@ -1,30 +1,39 @@
 package gettvinfo
 
 import (
-	"context"
 	"time"
 
-	"github.com/chromedp/chromedp"
+	"github.com/go-rod/rod"
 )
 
-func Scraping(url, selector string) (string, error) {
+// TV番組専用
+func Scraping(url, selector string) []string {
 
-	// contextの用意
-	ctx, _ := chromedp.NewContext(context.Background())
-	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
-	defer cancel()
+	// rodを使ってヘッドレスブラウザを立ち上げる
+	browser := rod.New().MustConnect()
+	defer browser.MustClose()
 
-	// htmlの取得
-	var html string
-	if err := chromedp.Run(ctx,
-		chromedp.Navigate(url),
-		chromedp.ScrollIntoView(`footer`),
-		chromedp.WaitVisible(selector),
-		// HTMLの取得
-		chromedp.OuterHTML("html", &html, chromedp.ByQuery),
-	); err != nil {
-		return "", err
+	// URLを開く
+	page := browser.MustPage(url)
+
+	// 20秒間URLを読み込めなかったら処理は強制終了
+	// MustWaitLoadでページが完全に表示されるまで待つ
+	page.Timeout(20 * time.Second).MustWaitLoad().CancelTimeout()
+
+	// 画面操作
+	page.MustElementR("button", "同意する").MustClick()
+	page.MustElementR("button", "スキップ").MustWaitStable().MustClick()
+	page.MustElement("button.button_button__GOl5m.modal_closeButton__4N3wA").MustWaitStable().MustClick()
+
+	// 要素を取得
+	var programs []string
+	sections := page.MustElements(selector)
+	for _, section := range sections {
+		s := section.MustText()
+		if s != "" {
+			programs = append(programs, s)
+		}
 	}
 
-	return html, nil
+	return programs
 }
