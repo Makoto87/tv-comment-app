3d135761b8e33aeb55aced41675e450169ee2b4b
Makoto87 <makosinhori@gmail.com>
2022-04-15 22:26:49 +0900

    add: unit test to insert programs and episodes into mysql

---
+++ go-app/gettvinfo/db_control_test.go
@@ -0,0 +1,55 @@
+package gettvinfo_test
+
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/Makoto87/tv-comment-app/go-app/gettvinfo"
+)
+
+func TestProgramInsert(t *testing.T) {
+	cases := []struct {
+		name        string
+		input       []string
+		mockClosure func(mock sqlmock.Sqlmock)
+	}{
+		{
+			name:  "test1",
+			input: []string{"hoge", "fuga"},
+			mockClosure: func(mock sqlmock.Sqlmock) {
+				mock.ExpectExec(`INSERT IGNORE INTO programs(program_name, created_at, updated_at) VALUES ("hoge", NOW(), NOW()),("fuga", NOW(), NOW())`).WillReturnResult(sqlmock.NewResult(2, 2))
+			},
+		},
+		{
+			name:  "test2",
+			input: []string{"hoge"},
+			mockClosure: func(mock sqlmock.Sqlmock) {
+				mock.ExpectExec(`INSERT IGNORE INTO programs(program_name, created_at, updated_at) VALUES ("hoge", NOW(), NOW())`).WillReturnResult(sqlmock.NewResult(1, 1))
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			// mock database を用意
+			db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+			defer db.Close()
+			gettvinfo.Db = db
+
+			c.mockClosure(mock)
+
+			// 関数実行
+			if err = gettvinfo.ProgramInsert(c.input); err != nil {
+				t.Errorf("error was not expected while insert programs: %s", err)
+			}
+
+			// mockと実際の関数を比較
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+		})
+	}
+}
--- go-app/gettvinfo/go.mod
+++ go-app/gettvinfo/go.mod
@@ -3,16 +3,13 @@ module github.com/Makoto87/tv-comment-app/go-app/gettvinfo
 go 1.18
 
 require (
-	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/go-rod/rod v0.105.1
 	github.com/go-sql-driver/mysql v1.6.0
-	github.com/google/go-cmp v0.5.7
 	github.com/joho/godotenv v1.4.0
-	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8
 )
 
 require (
-	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/ysmood/goob v0.4.0 // indirect
 	github.com/ysmood/gson v0.7.1 // indirect
 	github.com/ysmood/leakless v0.7.0 // indirect
