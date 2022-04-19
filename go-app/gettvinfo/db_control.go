8db34e85475c1f92cd3b18b6b0ce305424a89937
Makoto87 <makosinhori@gmail.com>
2022-04-19 19:17:20 +0900

    refactor: divide one function from EpisodeInsert function, that function is to get IDs from programs table

--- go-app/gettvinfo/db_control.go
+++ go-app/gettvinfo/db_control.go
@@ -63,47 +63,25 @@ func ProgramInsert(programs []string) error {
 	return nil
 }
 
-// insert episodes into database. First, get all id from programs. Second, insert episodes which use id from programs.
+// insert episodes into database. first, get all id from programs. second, insert episodes which use id from programs.
 func EpisodeInsert(programs []string) error {
-	selectID := `select id from programs where program_name in (`
-	vals := make([]any, 0, len(programs))
-	for _, p := range programs {
-		selectID += `?,`
-		vals = append(vals, p)
-	}
-	selectID = selectID[:len(selectID)-1] + `)`
-
-	stmt, err := DB.Prepare(selectID)
+	ids, err := selectID(programs)
 	if err != nil {
-		return fmt.Errorf("failed to prepare select program id: %w", err)
-	}
-
-	rows, err := stmt.Query(vals...)
-	if err != nil {
-		return fmt.Errorf("failed to Query id from programs where program_name in args: %w", err)
-	}
-	var ids []int
-	for rows.Next() {
-		var id int
-		if err := rows.Scan(&id); err != nil {
-			return fmt.Errorf("failed rows.Scan id from programs: %w", err)
-		}
-		ids = append(ids, id)
-	}
-	if err := rows.Err(); err != nil {
-		return fmt.Errorf("rows.Err : %w", err)
+		return nil
 	}
 
 	insert := "INSERT IGNORE INTO episodes(program_id, date, episode_number, episode_title, created_at, updated_at) VALUES "
 
-	vals = make([]any, 0, len(ids))
+	// vals = make([]any, 0, len(ids))
+	vals := make([]any, 0, len(ids))
 	for _, id := range ids {
 		insert += `(?, NOW(), null, null, NOW(), NOW()),`
 		vals = append(vals, id)
 	}
 	insert = insert[:len(insert)-1]
 
-	stmt, err = DB.Prepare(insert)
+	// stmt, err = DB.Prepare(insert)
+	stmt, err := DB.Prepare(insert)
 	if err != nil {
 		return fmt.Errorf("failed to prepare insert episode: %w", err)
 	}
@@ -112,4 +90,38 @@ func EpisodeInsert(programs []string) error {
 		return fmt.Errorf("failed to insert episode: %w", err)
 	}
 	return nil
+}
+
+// get all IDs of tv program which program_name in programs
+func selectID(programs []string) ([]int, error) {
+	s := `select id from programs where program_name in (`
+	vals := make([]any, 0, len(programs))
+	for _, p := range programs {
+		s += `?,`
+		vals = append(vals, p)
+	}
+	s = s[:len(s)-1] + `)`
+
+	stmt, err := DB.Prepare(s)
+	if err != nil {
+		return nil, fmt.Errorf("failed to prepare select program id: %w", err)
+	}
+
+	rows, err := stmt.Query(vals...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to Query id from programs where program_name in args: %w", err)
+	}
+	var ids []int
+	for rows.Next() {
+		var id int
+		if err := rows.Scan(&id); err != nil {
+			return nil, fmt.Errorf("failed rows.Scan id from programs: %w", err)
+		}
+		ids = append(ids, id)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("rows.Err : %w", err)
+	}
+
+	return ids, nil
 }
