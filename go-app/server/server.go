a3bf9210a8b53f0ce4cc6414b5ae257e3f15c40c
Makoto87 <makosinhori@gmail.com>
2022-04-29 22:43:57 +0900

    add: Graceful Shutdown when HTTP server is stopped

--- go-app/server/server.go
+++ go-app/server/server.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -32,6 +35,25 @@ func main() {
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 	http.Handle("/query", srv)
 
-	fmt.Println("GraphQL server is connected")
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	httpServer := &http.Server{
+		Addr: ":" + port,
+	}
+
+	go func() {
+		fmt.Println("GraphQL server is connecting")
+		if err := httpServer.ListenAndServe(); err != nil {
+			log.Fatalf("HTTP server ListenAndServe: %v", err)
+		}
+	}()
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+	<-sigint
+	fmt.Println("HTTP server Shutdown start")
+
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	if err := httpServer.Shutdown(timeoutCtx); err != nil {
+		log.Printf("HTTP server Shutdown: %v", err)
+	}
 }
