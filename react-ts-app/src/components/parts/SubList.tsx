f0b0b34666ee4e29449e532b62b54ef7ea6635a9
Makoto87 <makosinhori@gmail.com>
2022-05-03 09:08:54 +0900

    change: ctegory list is changed to hiragarna list.

--- react-ts-app/src/components/parts/SubList.tsx
+++ react-ts-app/src/components/parts/SubList.tsx
@@ -1,18 +1,20 @@
-import { Box, Heading, VStack, Button, Stack } from "@chakra-ui/react"
+import { Box, Heading, VStack, Button, Stack, HStack, Wrap, WrapItem } from "@chakra-ui/react"
 import { memo } from "react"
 
 export const SubList = memo(() => {
+      const hiraganaArray: String[] = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん".split('');
       return (
             <Box w='100%' >
-                  <Heading as='h3' size='md'>カテゴリ</Heading>
-                  <Stack direction={{ base: 'row', md: 'column' }} pt='3' spacing={3} >
-                        <Button colorScheme='blackAlpha' size='lg' w={{ md: '100%' }}>
-                              バラエティ
-                        </Button>
-                        <Button  colorScheme='blackAlpha' size='lg' w={{ md: '100%' }}>
-                              バラエティ
-                        </Button>
-                  </Stack>
+                  <Heading as='h3' size='md' pb={5}>50音検索</Heading>
+                  <Wrap w='100%' justify='space-around'>
+                        {hiraganaArray.map((char) => (
+                              <WrapItem w='25%' m={0}>
+                                    <Button colorScheme='blackAlpha' size='lg'>
+                                          {char}
+                                    </Button>
+                              </WrapItem>
+                        ))}
+                  </Wrap>
             </Box>
       )
 });
